437742c21ea73a49c5a6a9053f6b2725b582b6f3
amanharshx <amanharshx@gmail.com>
2026-01-29 06:02:16 +0530

    chore: testing releases

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { WelcomeScreen } from "@/components/welcome-screen";
 import { ConverterScreen } from "@/components/converter-screen";
 
 function App() {
-  const [showConverter, setShowConverter] = useState(false);
+  const  [showConverter, setShowConverter] = useState(false);
 
   if (showConverter) {
     return <ConverterScreen onBack={() => setShowConverter(false)} />;
